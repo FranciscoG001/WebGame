8c0c4b5311a8c406e4ac2b2492362c63a86fe231
Gonçalves <francisco-silva.goncalves@capgemini.com>
2023-12-29 22:44:25 +0000

    B-1 - Added first .db file

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ const db = new sqlite3.Database('batheoDb.db');
 
 // Crie uma tabela de usuários
 db.serialize(() => {
-  db.run("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, password TEXT)");
+  db.run("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, password TEXT, email TEXT)");
 });
 
 module.exports = db;
