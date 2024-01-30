00b0b89ebddb8f97bdbf4407784efea425c05e98
FranciscoG001 <kikogoncalves14@gmail.com>
2024-01-30 23:55:02 +0000

    First template of home page

--- src/FrontEnd/Home.js
+++ src/FrontEnd/Home.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import './Home.css';
 
 function Home() {
     return (
         <div className="HomePage">
+            {/* Top page */}
+            <div className="City"></div>
+            <div className="GridContainerResources">
+                <div className="GridItemResources"></div>
+                <div className="GridItemResources"></div>
+            </div>
+            <div className="GridContainerResources2">
+                <div className="GridItemResources2"></div>
+                <div className="GridItemResources2"></div>
+            </div>
+            <div className="GridContainerResources3">
+                <div className="GridItemResources3"></div>
+                <div className="GridItemResources3"></div>
+            </div>
 
+            {/* Bottom page */}
+            <div className="GridContainer">
+                <div className="GridItem"></div>
+                <div className="GridItem"></div>
+                <div className="GridItem"></div>
+                <div className="GridItem"></div>
+            </div>
+            <div className="Minorcircle"></div>
+            <div className="Minorcircle2"></div>
+            <div className="Minorcircle3"></div>
+            <div className="Season"></div>
+            <div className="DataTimeGame"></div>
+            <div className="Chat"></div>
         </div>
     );
 }
